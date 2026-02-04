7d4c02c8a288c93725d24bae02f01e9af85b7e96
Igal Vilensky <vilenskyigal@egmail.com>
2026-02-04 23:23:11 +0100

    feat: Integrate VueUse Motion module for animations.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@nuxtjs/i18n"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@nuxtjs/i18n", '@vueuse/motion/nuxt'],
   i18n: {
     strategy: "prefix_except_default",
     locales: [
